a2956876842a22b038620d647fddc4f19b3f81b6
Johnny Wang <johnnwa@microsoft.com>
2018-02-23 19:30:08 +0800

    jw c # lab

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student objStudent = new Student();
+            objStudent.Age = 2;
+
+            Console.WriteLine("my age is {0}", objStudent.Age);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Student
+    {
+        /// <summary>
+        /// 无参数的构造方法
+        /// </summary>
+        public Student()
+        { }
+
+        //学生ID
+        private int studentId;
+        public int StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+
+
+        //学生姓名
+        private string studentName;
+        public string StudentName
+        {
+            get { return studentName; }
+            set { studentName = value; }
+        }
+
+        //学生年龄
+        private int age =0;
+        public int Age
+        //{
+        //    get { return age; }
+        //    set { age = value; }
+        //}
+        {
+            get { return age; }
+            set {
+                if (age < 18)
+                    age = 18;
+                else
+                    age = value;
+            }
+        }
+    }
+}
